cccb9decbf3840348ac649fe7a244584d8fe4c59
mark <111102024+markmatakili2@users.noreply.github.com>
2024-02-13 22:18:47 +0300

    alco-shield

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import React from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
+import './App.css';
 import { useEffect, useState } from "react";
+import logo from './uplogo.png';
 
 function App() {
 
@@ -34,13 +37,30 @@ function App() {
 
 
   return (
-    <div className="App">
-      <h1>QR code scanner</h1>
-      {scanResult
-        ? <div>Success: <a href={"http://" + scanResult}>{scanResult}</a></div>
-        : <div id="reader"></div>
+    <div className="app-container">
+      <header className="header">
+        <div className="logo-container">
+          <img src={logo} alt="Logo" className="logo" />
+        </div>
+        <nav className="nav-menu">
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#faqs">FAQs</a></li>
+          </ul>
+        </nav>
+      </header>
 
-      }
+      { }
+
+      <div className="App">
+        <h1>QR code scanner</h1>
+        {scanResult
+          ? <div>Success: <a href={"http://" + scanResult}>{scanResult}</a></div>
+          : <div id="reader"></div>
+
+        }
+      </div>
 
     </div>
   )
